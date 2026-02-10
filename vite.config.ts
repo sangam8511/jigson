1ec2cf14465512c5ef99a967500eb5bf3159d6ab
Sangam Pandey <sangamp880@gmail.com>
2026-02-10 12:08:30 +0530

    Fix: Set conditional base path for dev and production

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/jigson/",
+  base: mode === "development" ? "/" : "/jigson/",
   server: {
     host: "::",
     port: 8080,
